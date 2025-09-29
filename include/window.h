e2cee2ce45d9b1d1062ce0ba1ec28cdfada47382
fern4ndomartins <fernando.martins.gouvea@gmail.com>
2025-09-29 02:40:24 -0300

    add window

---
+++ include/window.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+GLFWwindow* create_window(unsigned int, unsigned int);
---
+++ src/window.cc
@@ -0,0 +1,32 @@
+#include "../include/window.h"
+// handles window creation and glad
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+GLFWwindow* create_window(unsigned int scr_width, unsigned int scr_height) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(scr_width, scr_height, "window", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return NULL;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return 0;
+    }
+
+    return window;
+} 
